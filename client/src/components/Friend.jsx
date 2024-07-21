dbe050eb1a40badd0342f940eacb0425801fa305
kumarsarthak780 <141050286+kumarsarthak780@users.noreply.github.com>
2024-07-21 18:45:15 +0530

    Update Friend.jsx

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -18,12 +18,13 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
   const primaryDark = palette.primary.dark;
   const main = palette.neutral.main;
   const medium = palette.neutral.medium;
+  const url= "https://my-blog-backened.onrender.com"
 
   const isFriend = friends.find((friend) => friend._id === friendId);
 
   const patchFriend = async () => {
     const response = await fetch(
-      `http://localhost:3001/users/${_id}/${friendId}`,
+      `${url}/users/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
